73d95ce1133a4e1c57896aa3417b34df21e1dfc3
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2024-01-25 11:27:12 +0530

    fibonnaci

--- interview.js
+++ interview.js
@@ -1,11 +1,28 @@
 // palindorome
 
-const isPalinDrome = (x) => {
-  if (x < 0) return false;
-  return x === +x.toString().split("").reverse().join("");
-};
+// const isPalinDrome = (x) => {
+//   if (x < 0) return false;
+//   return x === +x.toString().split("").reverse().join("");
+// };
 
-const res = isPalinDrome(114);
-console.log(res);
+// const res = isPalinDrome(114);
+// console.log(res);
 
+// Fabonacci Number at given index
 
+// const fib = (n) => {
+//   const arr = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     arr.push(arr[i - 1] + arr[i - 2]);
+//   }
+//   return arr[n];
+
+// recursion
+// if (n <= 1) {
+//   return n;
+// }
+// return fib(n - 1) + fib(n - 2);
+// };
+
+// const res = fib(8);
+// console.log(res);
